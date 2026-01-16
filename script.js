093d16bc8b09226b271f85d77c39c05d2cf6e5c2
celineWan1210 <wanyingxuan1210@gmail.com>
2026-01-16 16:45:25 +0800

    Update add substract multiple and divide function, tested

---
+++ script.js
@@ -0,0 +1,25 @@
+// add function
+function add(a, b) {
+    return a + b;
+}
+
+// subtract function
+function subtract(a, b) {
+    return a - b;
+}
+
+// multiple function
+function multiple(a, b) {
+    return a * b;
+}
+
+// divide fuction
+function divide(a, b) {
+    return a / b;
+}
+
+// test
+console.log(add(2, 3));
+console.log(subtract(2, 3));
+console.log(multiple(2, 3));
+console.log(divide(6,3));
